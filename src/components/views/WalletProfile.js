9ad588123d20e42da8d24162e8fc823c88016184
Shayan <admin@shayawn.com>
2024-02-12 18:16:59 +0000

    ETH price added to wallet profile

--- src/components/views/WalletProfile.js
+++ src/components/views/WalletProfile.js
@@ -1,13 +1,35 @@
 import { Row,Col } from 'antd';
-import {useState} from 'react';
+import {useState,useEffect} from 'react';
+import axios from 'axios';
 import {AiOutlineQrcode,AiTwotoneContainer,AiOutlineKey,AiOutlineUser} from 'react-icons/ai';
 import WalletResetPasswordModal from "../component/WalletResetPasswordModal";
 import { useTranslation } from 'react-i18next';
+import { SERVER_URL } from "../../constants/env";
     
 function WalletProfile() {
-  const [t,i18n] = useTranslation();
-  const [use,setUser] = useState(JSON.parse(localStorage.getItem("userInfo")));
-  const [showModal,setShowModal] = useState(false);
+    const [t,i18n] = useTranslation();
+    const [use,setUser] = useState(JSON.parse(localStorage.getItem("userInfo")));
+    const [showModal, setShowModal] = useState(false);
+    const [ethPrice, setEthPrice] = useState(null);
+
+    const fetchEthPrice = async () => {
+        try {
+            const response = await axios.get(SERVER_URL + 'wallets/gettoptokens');
+            if (response.data.response && response.data.data.length > 0) {
+                const ethData = response.data.data.find(token => token.symbol === 'ETH');
+                if (ethData) {
+                    setEthPrice(ethData.price);
+                }
+            }
+        } catch (error) {
+            console.error("Error fetching ETH price:", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchEthPrice();
+    }, []);
+
   return (
     <Col span={22} offset={1} className="mt-8 myColor1 text-sm">
       <Row>
@@ -34,6 +56,12 @@ function WalletProfile() {
           <WalletResetPasswordModal  setModalShow={setShowModal} title="Reset Password"/>
         : null
       }
+      {/* Display ETH Price */}
+      {ethPrice && (
+          <div>
+              <p>Current ETH Price: {ethPrice}</p>
+          </div>
+      )}
     </Col>
   );
 }
